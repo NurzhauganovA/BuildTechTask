287767b7b826ddf0153042900c129caeab7cd239
NurzhauganovA <90419990+NurzhauganovA@users.noreply.github.com>
2023-02-03 19:39:18 +0600

    Authorization with JWT token. Frontend Part

--- backend/orderDjangoApp/orderDjangoApp/settings.py
+++ backend/orderDjangoApp/orderDjangoApp/settings.py
@@ -140,3 +140,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000"
 ]
+
+
+CORS_ORIGIN_ALLOW_ALL = True
+Access_Control_Allow_Credentials = True
